7020fad6dbc1850e29afdde37464f47086819dd1
徐铭ethan <xuming@theduapp.com>
2020-11-25 14:17:49 +0800

    feat(11): 增加更精简的代码形式

--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -9,22 +9,32 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
   let ans = 0;
   let left = 0;
   let right = height.length - 1;
 
   while (left < right) {
-    const area = Math.min(height[left], height[right]) * (right - left);
+    const minHeight =
+      height[left] < height[right] ? height[left++] : height[right--];
+    const area = minHeight * (right - left + 1);
     ans = Math.max(ans, area);
-
-    if (height[left] < height[right]) {
-      left++
-    } else {
-      right--
-    }
   }
   return ans;
+};
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  for (let i = 0, j = height.length - 1; i < j;) {
+    const minHeight = height[i] < height[j] ? height[i++] : height[j--];
+    const area = minHeight * (j - i + 1);
+    max = Math.max(max, area);
+  }
+  return max;
 };
 
 // @lc code=end
